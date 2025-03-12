cd622b73b636551d519e6d1d01f53f3f67075b33
allie <aferas@anthology.com>
2025-03-12 14:33:34 -0400

    fixes to company map logic, create map objects, some filename updates

--- src/mapping.py
+++ src/mapping.py
@@ -9,27 +9,17 @@ def norm_string(s):
     if isinstance(s,str):
         s = s.lower().replace('&','and')
         s = re.sub(r'[^\w\d\s]','',s)
+        s = re.sub(r'\s+',' ',s)
         return s
     return ''
 
 class CompanyMap:
-    def __init__(self, target_data, name_cols=['NAME','DBA'], addr_col = 'ADDRESS', fuzzy_alg='ratio'):
+    def __init__(self, target_data, name_cols=['NAME','DBA'], addr_col = 'ADDRESS'):
         self._set_target(target_data,name_cols,addr_col)
-
-        FUZZY_DICT = {
-            'ratio': fuzz.ratio,
-            'partial_ratio': fuzz.partial_ratio,
-            'token_sort_ratio': fuzz.token_sort_ratio,
-            'token_set_ratio': fuzz.token_set_ratio
-        }
-        try:
-            self.fuzzy_alg = FUZZY_DICT[fuzzy_alg]
-        except KeyError:
-            raise ValueError(f'Invalid fuzzy algorithm: {fuzzy_alg}. Must be one of {list(FUZZY_DICT.keys())}')
     
-    def _score(self, x, y):
+    def _score(self, x, y, fuzzy_alg=fuzz.partial_ratio):
         if (len(x)>0) & (len(y)>0):
-            return self.fuzzy_alg(x,y)
+            return fuzzy_alg(x,y)
         return np.nan
 
     def _set_target(self, X, name_cols, addr_col):
@@ -54,11 +44,22 @@ class CompanyMap:
                 namescore.append(np.vstack(self.learned_data[c].apply(lambda x: self._score(x, n)).values))
         namescore = np.nanmax(np.concatenate(namescore,axis=1),axis=1)
 
+        namescore_full = []
+        for n in names:
+            for c in self.name_cols:
+                namescore_full.append(np.vstack(self.learned_data[c].apply(lambda x: self._score(x, n, fuzz.ratio)).values))
+        namescore_full = np.nanmax(np.concatenate(namescore_full,axis=1),axis=1)
+
+        addrscore_full = self.learned_data[self.addr_col].apply(lambda x: self._score(x, address, fuzz.ratio))
         addrscore = self.learned_data[self.addr_col].apply(lambda x: self._score(x, address))
 
-        score = np.nanmean([namescore, addrscore],axis=0)
+        score = np.nanmean([namescore_full, addrscore_full],axis=0)
 
-        return list(self.learned_data[(score >= avg_threshold) & ((namescore >= threshold) | (addrscore >= threshold))].index)
+        return list(self.learned_data[
+            (namescore>=threshold) & ((addrscore.isna())|(addrscore>=threshold))
+            #| (addrscore_full>=threshold)
+            | ((score>=avg_threshold) & ((namescore_full>=threshold)|(addrscore_full>=threshold)))
+        ].index)
     
     def get_match_df(self, names = [], address = '', threshold=95, avg_threshold=80):
         idx = self.get_match_idx(names, address, threshold, avg_threshold)
@@ -66,16 +67,17 @@ class CompanyMap:
     
 def fuzzy_join(
         left_df, 
+        left_name_cols,
+        left_addr_col,
         right_df, 
-        name_cols, 
-        addr_col, 
+        right_name_cols,
+        right_addr_col, 
         how='left', 
         threshold=95, 
         avg_threshold=80, 
-        fuzzy_alg='ratio',
     ):
-    c = CompanyMap(right_df, name_cols, addr_col, fuzzy_alg)
-    matches = left_df.apply(lambda x: c.get_match_idx(x[name_cols], x[addr_col], threshold, avg_threshold), axis=1)
+    c = CompanyMap(right_df, right_name_cols, right_addr_col)
+    matches = left_df.apply(lambda x: c.get_match_idx(x[left_name_cols], x[left_addr_col], threshold, avg_threshold), axis=1)
     df = pd.concat([left_df, pd.DataFrame(matches.tolist())],axis=1).melt(id_vars=left_df.columns)
     df = df.set_index('value',drop=False).join(c.learned_data,lsuffix='_l',rsuffix='_r',how=how)
     return df
